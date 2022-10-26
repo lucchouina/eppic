7b7084f9604e7cce7695270f24420d425568d005
Luc Chouinard <lchouinard@juniper.net>
2022-10-26 10:11:46 -0700

    Update the crash code for 5.0 kernels

--- applications/crash/code/include/stddef.h
+++ applications/crash/code/include/stddef.h
@@ -1,16 +1,10 @@
 #ifndef _LINUX_STDDEF_H
 #define _LINUX_STDDEF_H
 
-
-enum {
-	false	= 0,
-	true	= 1
-};
-
 /* extensive use of prefetch. Eliminate from parsing. */
 
 #define prefetch(x) (1)
 
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#define offsetof(type, member) ((size_t) &((type *)0)->member)
 
 #endif
--- applications/crash/code/wait.c
+++ applications/crash/code/wait.c
@@ -1,8 +1,8 @@
 #include <list.h>
 void waiters(wait_queue_head_t *q)
 {
-wait_queue_t *curr, *next;
-int i=0;
+struct wait_queue_entry *curr, *next;
+
     list_for_each_entry_safe(curr, next, &q->task_list, task_list) {
         if(curr->func == &ep_poll_callback) {
             printf("Epoll\n");
